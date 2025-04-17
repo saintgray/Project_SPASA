15da1dd37de323d45c17f7bb7b6353a2395fdcd3
saintgray <saintgray0@gmail.com>
2025-04-17 22:44:37 +0900

    test 자원 삭제

--- spasa_manage/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import spasa_manage # views.py import
-
-urlpatterns=[
-    # execute members() function in view.py when url pattern is  %root%/members/
-    # project folder will be moduled named folder name
-    # 
-    path('home/', spasa_manage.get_view_permission, name='spasa_manage'),
-]
